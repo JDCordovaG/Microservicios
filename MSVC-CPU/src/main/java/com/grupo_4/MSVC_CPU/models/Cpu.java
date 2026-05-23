bcda89bad2e4c68b32abdf136ceb505d86b17ebb
Joan Lara <jo.larab@duocuc.cl>
2026-05-23 17:59:35 -0400

    Se actualizo el modelo CPU y se añadio su DTO
    - Joan Lara

--- MSVC-CPU/src/main/java/com/grupo_4/MSVC_CPU/models/Cpu.java
+++ MSVC-CPU/src/main/java/com/grupo_4/MSVC_CPU/models/Cpu.java
@@ -1,66 +1,47 @@
 package com.grupo_4.MSVC_CPU.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Entity
-@Table(name = "cpu")
-@Getter
-@Setter
-@ToString
+@Data
 @NoArgsConstructor
-
-
+@Entity
+@Table(name = "cpus")
 public class Cpu {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cpu_id")
-    private Long cpuId;
+    private Long id;
+
+    @Column(nullable = false, name = "componente_id")
+    private Long componenteId;
 
     @Column(nullable = false, name = "socket_cpu")
-    @NotBlank(message = "El campo socket no puede estar vacio")
-    private Long marca;
-
-    @Column(nullable = false, name = "generacion_cpu")
-    @NotBlank(message = "El campo generacion ghz no puede estar vacio")
-    private Long generacion;
+    private String socket;
 
     @Column(nullable = false, name = "nucleos_cpu")
-    @NotBlank(message = "El campo nucleos no puede estar vacio")
-    private Long nucleos;
+    private Integer nucleos;
 
     @Column(nullable = false, name = "hilos_cpu")
-    @NotBlank(message = "El campo hilos no puede estar vacio")
-    private Long hilos;
+    private Integer hilos;
 
     @Column(nullable = false, name = "frecuencia_base_cpu")
-    @NotBlank(message = "El campo frecuencia base no puede estar vacio")
-    private Long frecuenciaBase;
+    private Double frecuenciaBase;
 
     @Column(nullable = false, name = "frecuencia_turbo_cpu")
-    @NotBlank(message = "El campo frecuencia turbo no puede estar vacio")
-    private Long frecuenciaTurbo;
+    private Double frecuenciaTurbo;
 
     @Column(nullable = false, name = "tdp_watts_cpu")
-    @NotBlank(message = "El campo tdp watts no puede estar vacio")
-    private Long tdpWatts;
+    private Integer tdpWatts;
 
     @Column(nullable = false, name = "soporta_ddr4_cpu")
-    @NotBlank(message = "El campo soporta ddr4 no puede estar vacio")
-    private Long soportaDdr4;
-
+    private Boolean soportaDdr4;
     @Column(nullable = false, name = "soporta_ddr5_cpu")
-    @NotBlank(message = "El campo generacion ghz no puede estar vacio")
-    private Long soportaDdr5;
+    private Boolean soportaDdr5;
 
-
-
-
-
-
+    @Column(nullable = false)
+    private String estado;
 
 }
